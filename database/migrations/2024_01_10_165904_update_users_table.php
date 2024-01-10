e4c9093f92e65639bdc1ab51fe11d9aaafb99e7c
Rizal M <rizalmadani70@gmail.com>
2024-01-10 17:45:02 +0700

    Update migrasi tabel users

--- database/migrations/2024_01_10_165904_update_users_table.php
+++ database/migrations/2024_01_10_165904_update_users_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['admin', 'author'])
+                ->after('id');
             $table->string('phone')
                 ->after('name');
             $table->string('address')
