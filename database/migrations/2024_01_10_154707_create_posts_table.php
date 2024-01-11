83c8d77cd50d63fb3e87f58a0db9c0e3baa0331a
Rizal M <rizalmadani70@gmail.com>
2024-01-11 19:40:06 +0700

    Update migrasi

--- database/migrations/2024_01_10_154707_create_posts_table.php
+++ database/migrations/2024_01_10_154707_create_posts_table.php
@@ -26,7 +26,6 @@ return new class extends Migration
             $table->string('banner')
                 ->nullable();
             $table->text('body');
-            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
