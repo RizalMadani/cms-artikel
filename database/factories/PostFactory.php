0ab704d163601115c51d0b311d549ac8cde9149a
Rizal M <rizalmadani70@gmail.com>
2024-01-12 19:10:35 +0700

    Tambah excerpt dan banner dummy

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -22,7 +22,10 @@ class PostFactory extends Factory
             'user_id' => User::factory(),
             'category_id' => Category::factory(),
             'title' => fake()->words(rand(3, 7), true),
-            'body' => fake()->paragraphs(rand(3, 6), true),
+            'excerpt' => $this->faker->sentence(10),
+            'body' => collect($this->faker->paragraphs(mt_rand(5, 10)))
+                        ->map(fn($p) => "<p>$p</p>")
+                        ->implode(''),
         ];
     }
 }
--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -13,19 +13,22 @@
             @foreach ($posts as $post)
                 <div class="col-md-4 d-flex">
                     <div class="blog-entry justify-content-end">
-                        <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');"></a>
+                        <a href="blog-single.html" class="block-20" style="background-image: url('{{
+                            $post->banner ? asset('storage/post_banner/'.$post->banner) : '/landing-page/images/gambar_bebas.png'
+                        }}');"></a>
+
                         <div class="text">
                             <div class="d-flex align-items-center mb-4 topp">
                                 <div class="one">
-                                    <span class="day">11</span>
+                                    <span class="day">{{ date('d', strtotime($post->updated_at)) }}</span>
                                 </div>
                                 <div class="two">
-                                    <span class="yr">2020</span>
-                                    <span class="mos">September</span>
+                                    <span class="yr">{{ date('Y', strtotime($post->updated_at)) }}</span>
+                                    <span class="mos">{{ date('F', strtotime($post->updated_at)) }}</span>
                                 </div>
                             </div>
                             <h3 class="heading"><a href="#">{{ $post->title }}</a></h3>
-                            <p>{{ Str::limit($post->body, 50, '...') }}</p>
+                            <p>{{ $post->excerpt }}</p>
                             <p><a href="#" class="btn btn-primary">Read more</a></p>
                         </div>
                     </div>
