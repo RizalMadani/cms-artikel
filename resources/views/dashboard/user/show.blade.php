8db7e24046529f414e41aec97414313171c5cf59
Rizal M <rizalmadani70@gmail.com>
2024-01-15 19:21:27 +0700

    Update halaman detail user

--- resources/views/dashboard/user/show.blade.php
+++ resources/views/dashboard/user/show.blade.php
@@ -57,13 +57,5 @@
             </div>
         </div>
     </div>
-
-    <div class="col-lg-6">
-        <div class="card">
-            <div class="card-body">
-
-            </div>
-        </div>
-    </div>
 </div>
 @stop
