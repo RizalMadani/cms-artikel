5cd08d49de990128450b818f8b5d216f80c36b67
Rizal M <rizalmadani70@gmail.com>
2024-01-15 17:13:29 +0700

    Delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('dashboard.user.index', [
             'users' => User::all(),
         ]);
+    }
+
+    public function destroy(User $user)
+    {
+        User::destroy($user->id);
+
+        Session::flash('alert-class', 'success');
+        Session::flash('alert', ['Berhasil',  'Berhasil menghapus user']);
+
+        return redirect()->route('dashboard.user.index');
     }
 }
